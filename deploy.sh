751466d645a9f0d2c86014601f3721e55bb88b05
DEEPTHI-92 <151435086+DEEPTHI-92@users.noreply.github.com>
2024-02-08 11:50:22 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ read -s -p "Enter Docker Hub password: " PASSWORD
 echo "$PASSWORD" | docker login $REGISTRY_URL -u $USERNAME --password-stdin
 unset PASSWORD
 docker-compose up -d
-docker tag nodejs deepthiarmour/react-node
+docker tag nodejs deepthiarmour/dev
 
-docker push deepthiarmour/react-node
+docker push deepthiarmour/dev
 
