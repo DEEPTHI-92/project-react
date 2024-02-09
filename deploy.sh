a5f4830fa2289dafd008fd76785040b6ecba49e7
DEEPTHI-92 <151435086+DEEPTHI-92@users.noreply.github.com>
2024-02-09 17:56:42 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@
 
 REGISTRY_URL="https://index.docker.io/v1/"
 USERNAME="deepthiarmour"
-read -s -p "Enter Docker Hub password: " PASSWORD
+PASSWORD="dckr_pat_3b3_K-a-aEoJgFegBCo9cAp6n0Q"
 echo "$PASSWORD" | docker login $REGISTRY_URL -u $USERNAME --password-stdin
-unset PASSWORD
+
 docker-compose up -d
 docker tag nodejs deepthiarmour/prod
 
